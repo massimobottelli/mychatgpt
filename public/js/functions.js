9da331cc4beeaa50e03627cb1897bbd1df7071b6
Massimo Bottelli <88494940+massimobottelli@users.noreply.github.com>
2023-04-18 18:54:16 +0200

    Add files via upload

---
+++ public/js/functions.js
@@ -0,0 +1,17 @@
+// auto adjuste height of textarea
+textarea = document.querySelector("#prompt");
+textarea.addEventListener('input', autoResize, false);
+    
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
+
+// make return in textarea to submit form
+textarea.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13 && !event.shiftKey) {
+    event.preventDefault();
+    event.target.form.submit();
+  }
+});
+
